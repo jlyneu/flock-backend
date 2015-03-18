9735f03f1fd2c468c6c4afb00765e92f7ef0003a
jlyneu <jly@ccs.neu.edu>
2015-03-17 23:11:36 -0400

    Added Access-Control-Allow-Methods

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:63342/", "http://zachbachiri.com", "http://northeastern.edu");
+  res.header("Access-Control-Allow-Methods", "GET");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
