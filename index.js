a615204cf750122094cf71f90eb5b77fc2b9a093
jlyneu <jly@ccs.neu.edu>
2015-03-17 22:27:42 -0400

    Added authentication and search tweets methods
    -Added method to get request token and redirect user to
    Twitter login page
    -Added method to use oauth_token and oauth_verifier to
    get Twitter access token and token secret
    -Added method to accept query parameters from front end
    and return tweet query results
    -Add library to generate uuids as session ids

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+// imports library to create uuids used as session ids
+var uuid = require('node-uuid');
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:63342");
+  res.header("Access-Control-Allow-Origin", "http://localhost:63342/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -14,13 +17,13 @@ var twitterAPI = require('node-twitter-api');
 var twitter = new twitterAPI({
     consumerKey: 'pEaf5TgKTpz0Tf1M9uyqZSysQ',
     consumerSecret: 'dTV7OuEkgauN8syVrOT5T9XzK8CnXpSvjMEELlZshz1aqdsAVW',
-    callback: 'http://zachbachiri.com/Flock/#redirect'
+    callback: 'http://zachbachiri.com/Flock/#/redirect'
 });
 
-var requestToken;
-var requestTokenSecret;
+// keys: session IDs -> values: {accessToken:..., accessTokenSecret:...}
+var sessions = {}
 
-app.get('/', function(request, response) {
+app.get('/requestToken', function(request, response) {
     twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results) {
         if (error) {
             console.log("Error getting OAuth request token : " + error);
@@ -35,12 +38,12 @@ app.get('/', function(request, response) {
         }
     });
 });
-/*
-app.get('/getAuthToken', function(
-*/
-/*
 
-twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
+app.get('/accessToken', function(request, response) {
+    var oauth_token = request.query.oauth_token;
+    var oauth_verifier = request.query.oauth_verifier;
+    
+    twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
     if (error) {
         console.log(error);
     } else {
@@ -54,12 +57,39 @@ twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, functio
                 //accessToken and accessTokenSecret can now be used to make api-calls (not yet implemented)
                 //data contains the user-data described in the official Twitter-API-docs
                 //you could e.g. display his screen_name
-                console.log(data["screen_name"]);
+                console.log(data);
+                console.log(response);
+                var accessInfo = {
+                                     'accessToken': accessToken,
+                                     'accessTokenSecret': accessTokenSecret
+                                 }
+				var sessionId = uuid.v4();
+				sessions[uuid.v4()] = accessInfo;
+                response.send({ 'sessionId': sessionId });
             }
         });
     }
 });
-*/
+
+app.get('/tweets', function(request, response) {
+
+	// TODO: Return error if sessionId not included in request
+
+    var sessionId = request.query.sessionId;
+    var sessionInfo = sessions[sessionId];
+    delete request.query.sessionId;
+    
+    twitter.search('tweets', 
+        request.query, 
+        sessionInfo.accessToken,
+        sessionInfo.accessTokenSecret,
+        function(error, data, twitterResponse){
+            // TODO: return appropriate error if call fails
+            response.send(twitterResponse);
+    	}
+    );
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
